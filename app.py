9202b7767eadc4df6510f9726bbc307be4ae5e7e
Whale <zutterjostar@gmail.com>
2025-08-28 23:56:24 +0700

    Fixed datetime import error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+#from datetime import datetime
 import datetime
 import os
 from functools import wraps
