57e13ccf66a21e88fca32cc8a46b201657604460
Whale <zutterjostar@gmail.com>
2025-08-28 23:10:10 +0700

    Fixed utcnow AttributeError by correcting method call

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ class Booking(db.Model):
     end_date = db.Column(db.Date, nullable=False)
     total_price = db.Column(db.Float, nullable=False)
     status = db.Column(db.String(20), nullable=False, default='confirmed')
-    booked_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    booked_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return f'<Booking {self.id} - {self.vendor_name} for Stall {self.stall.name}>'
